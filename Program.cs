85ae65c53667cdfb3db440fc8ab5326a62a80013
Mario Berger <m.berger@grauepixel.de>
2021-11-28 13:15:07 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello_mario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mario!");
+            Console.ReadLine();
+        }
+    }
+}
